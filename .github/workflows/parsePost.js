b67b9c12b5e7dc1050f5bb94f63295bf3fbd6faf
Risa Watanabe Harada <supikateriburu@gmail.com>
2024-11-03 10:21:14 +0900

    tmp

--- .github/workflows/parsePost.js
+++ .github/workflows/parsePost.js
@@ -10,6 +10,22 @@ async function parsePost() {
     console.log(latestEpisode);
     console.log(latestEpisode.title);
     console.log(latestEpisode.enclosure_url);
+
+    const extractedUUID = extractUUID(latestEpisode.enclosure_url);
+    console.log(extractedUUID);
+
+
+}
+
+function extractUUID(url) {
+    // UUIDの正規表現パターン
+    const uuidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i;
+
+    // URLからUUIDを抽出
+    const match = url.match(uuidRegex);
+
+    //
+    return match ? match[0] : null;
 }
 
 parsePost();
