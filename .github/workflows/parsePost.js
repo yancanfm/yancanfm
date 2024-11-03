b444c3f5f1e26a0a2e6b921e6c3843f2bc1a64d0
Risa Watanabe Harada <supikateriburu@gmail.com>
2024-11-03 09:50:52 +0900

    tmp

--- .github/workflows/parsePost.js
+++ .github/workflows/parsePost.js
@@ -1,8 +1,12 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
-const feed = await parser.parseURL('https://example.com/feed.xml');
+async function parsePost() {
+    const Parser = require('rss-parser');
+    const parser = new Parser();
+    const feed = await parser.parseURL('https://example.com/feed.xml');
 
-feed.items.forEach(item => {
-    console.log(item.title);
-    console.log(item.link);
-});
+    feed.items.forEach(item => {
+        console.log(item.title);
+        console.log(item.link);
+    });
+}
+
+parsePost();
