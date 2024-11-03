01c320f15869a9aa226568d258d1c51f4aa04576
Risa Watanabe Harada <supikateriburu@gmail.com>
2024-11-03 11:06:18 +0900

    tmp

--- .github/workflows/parsePost.js
+++ .github/workflows/parsePost.js
@@ -11,8 +11,15 @@ async function parsePost() {
     console.log(latestEpisode.enclosure.url);
 
     const uuidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i;
-    const match = latestEpisode.enclosure.url.match(uuidRegex);
-    console.log(match ? match[0] : "No match found");
+    const url = latestEpisode.enclosure.url.match(uuidRegex);
+    console.log(url ? url[0] : "No match found");
+
+    const fs = require('fs');
+
+    fs.writeFile('hello.txt', 'Hello, world!', (err) => {
+        if (err) throw err;
+        console.log('File has been created and saved!');
+    });
 }
 
 parsePost();
