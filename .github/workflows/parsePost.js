d30f9b8da2f9e077ad1bae3b43c00e5893430e12
risacan <supikateriburu@gmail.com>
2024-11-03 12:08:37 +0900

    🔧 Add new workflow (#75)
    * 🔧 Add new workflow
    
    * 🔥 remove npm ci
    
    * 🔧 Get RSS
    
    * 🔧 Fix indent
    
    * 🔧 Add "
    
    * 🔧 Install
    
    * 🔧 Fix code
    
    * 🔧 Fix indent
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * 🆕 New episode!
    
    * tmp
    
    * 🆕 New episode!
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * 🆕 New episode!
    
    * tmp
    
    * 🆕 New episode!
    
    * tmp
    
    * 🆕 New episode!
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .github/workflows/parsePost.js
@@ -0,0 +1,29 @@
+async function parsePost() {
+    const Parser = require('rss-parser');
+    const parser = new Parser();
+    const feed = await parser.parseURL('https://rss.art19.com/yancanfm');
+
+    console.log("🐶")
+    const latestEpisode = feed.items[0];
+    console.log(JSON.stringify(latestEpisode.enclosure, null, 2));
+
+    const titleNumber = latestEpisode.title.match(/\d+/g);
+    const title = latestEpisode.title
+    const enclosure_url = latestEpisode.enclosure.url
+    const uuidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i;
+    const urlId = enclosure_url.match(uuidRegex);
+    const content = latestEpisode.content
+    const dateString = latestEpisode.pubDate
+    const date = new Date(dateString);
+    const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+    const audioFilePath = `https://art19.com/shows/df57f7bd-37e4-465d-90a7-5c18294f290b/episodes/${urlId}/embed`
+
+    const fs = require('fs');
+
+    fs.writeFile(`_posts/${formattedDate}-${titleNumber}.md`, `---\nactor_ids:\n  - rihoyan\n  - risacan\ntitle: '${title}'\ndate: ${formattedDate}\nlayout: art19\nenclosure_url: ${enclosure_url}\naudio_file_path: ${audioFilePath}\n---\n${content.replace(/        /, "")}`, (err) => {
+        if (err) throw err;
+        console.log('File has been created and saved!');
+    });
+}
+
+parsePost();
