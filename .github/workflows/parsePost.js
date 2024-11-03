b361146e1912c4b3a4909e97e561953ba75fd98f
Risa Watanabe Harada <supikateriburu@gmail.com>
2024-11-03 10:41:05 +0900

    tmp

--- .github/workflows/parsePost.js
+++ .github/workflows/parsePost.js
@@ -5,14 +5,13 @@ async function parsePost() {
 
     console.log("🐶")
     const latestEpisode = feed.items[0];
-    console.log("🐱")
     console.log(JSON.stringify(latestEpisode.enclosure, null, 2));
     console.log(latestEpisode);
     console.log(latestEpisode.title);
-    console.log(latestEpisode.enclosure_url);
+    console.log(latestEpisode.enclosure.url);
 
     const uuidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i;
-    const match = latestEpisode.enclosure_url.match(uuidRegex);
+    const match = latestEpisode.enclosure.url.match(uuidRegex);
     console.log(match ? match[0] : "No match found");
 }
 
